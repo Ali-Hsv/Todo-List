4f1d6e911f58615f7c96a34e0fe030b273cb2ca8
Ali Hasanov <nan.fgbs@gmail.com>
2025-09-27 23:36:17 +0400

    chore: deploy project from GithubPages - pt2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'; // если React
 // import vue from '@vitejs/plugin-vue'; // если Vue
 
 export default defineConfig({
-  base: '/REPO_NAME/', // <--- имя твоего репозитория
+  base: '/Todo-List/', // <--- имя твоего репозитория
   plugins: [react()],
 });
